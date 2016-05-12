72a57a0536f0286f7056e828280c8a59e0e18ba2
Fabian Vergara <vergarf@gmail.com>
2016-05-12 19:41:25 -0400

    Getting app ready

--- Controlling Keyboard/ViewController.swift
+++ Controlling Keyboard/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userInputField: UITextField!
+    @IBOutlet var outputLabel: UILabel!
+    
+    
+    @IBAction func testButtonListener(sender: AnyObject) {
+        outputLabel.text = userInputField.text
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
