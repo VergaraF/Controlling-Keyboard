075ad7427dd432134cf6a48a2715cb5ba8177299
Fabian Vergara <vergarf@gmail.com>
2016-05-12 20:29:08 -0400

    Did the controlling keyboard feature

--- Controlling Keyboard/ViewController.swift
+++ Controlling Keyboard/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var userInputField: UITextField!
     @IBOutlet var outputLabel: UILabel!
@@ -18,16 +18,25 @@ class ViewController: UIViewController {
         outputLabel.text = userInputField.text
     }
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.userInputField.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool{
+        textField.resignFirstResponder()
+        return true
     }
 
 
